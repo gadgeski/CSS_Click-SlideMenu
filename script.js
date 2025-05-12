ca7bfcdbb123718d8971947114846985c0dde4a6
gadgeski <xtwh.ce5bil@gmail.com>
2025-05-13 00:18:16 +0900

    Updated script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const btnOpen = document.querySelector("#menu-open");
 const btnClose = document.querySelector("#menu-close");
 const menuPanel = document.querySelector(".menu-container");
+
+// メニューボタンクリック時
+btnOpen.addEventListener("click", () => {
+  menuPanel.classList.add("pannel-open");
+});
+
+// 閉じるボタンクリック時
+btnClose.addEventListener("click", () => {
+  menuPanel.classList.remove("panel-open");
+});
