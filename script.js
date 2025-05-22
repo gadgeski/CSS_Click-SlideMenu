07effcc8813fc5f5233c479d2ab0d7b30c9b4752
gadgeski <xtwh.ce5bil@gmail.com>
2025-05-22 21:55:41 +0900

    Updated index.html and more

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ btnOpen.addEventListener("click", () => {
 btnClose.addEventListener("click", () => {
   menuPanel.classList.remove("panel-open");
 });
+
+// カーソルの位置で変色
+const btn = document.querySelector("#flow-button-js");
+btn.addEventListener("mousemove", (e) => {
+  const rect = btn.getBoundingClientRect();
+  const x = ((e.clientX - rect.left) / rect.width) * 100;
+  // カーソル位置の計算
+  btn.style.backgroundPosition = `${x}% 0`;
+  // 背景位置を動的に
+});
+btn.addEventListener("mouseleave", () => {
+  btn.Style.backgroundPosition = "0% 0";
+});
+// マウスを離した時
